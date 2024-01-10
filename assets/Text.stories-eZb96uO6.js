d6fe18dc3c7f362ca09a307bfa29526fe5b9c98f
Ayiga <Ayiga@users.noreply.github.com>
2024-01-10 22:17:33 +0000

    deploy: 6279c5a890c72c4c26ca22ad60d05a1e8bd154a0

---
+++ assets/Text.stories-eZb96uO6.js
@@ -0,0 +1,7 @@
+const t=r=>r.text;try{t.displayName="Text",t.__docgenInfo={description:`Text simply represents a Text node without any other details. The text
+passed into the Text element will be presented as is without change or
+adjustments.`,displayName:"Text",props:{text:{defaultValue:null,description:"",name:"text",required:!0,type:{name:"string"}}}}}catch{}const a={title:"Components/Text",component:t},e={args:{text:"Hello, World!"}};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    text: 'Hello, World!'
+  }
+}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const l=["Simple"];export{e as Simple,l as __namedExportsOrder,a as default};
---
+++ src/components/text/Text.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+export type TextProps = {
+    text: string;
+};
+/**
+ * Text simply represents a Text node without any other details. The text
+ * passed into the Text element will be presented as is without change or
+ * adjustments.
+ */
+declare const Text: React.FC<TextProps>;
+export default Text;
