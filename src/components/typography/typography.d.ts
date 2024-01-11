2725145df517ac97f8587d2f5f75ca5c39816ef3
Ayiga <Ayiga@users.noreply.github.com>
2024-01-11 15:07:22 +0000

    deploy: 383cd6939e75bc4d593d9abcf5ccb7b148667916

---
+++ src/components/typography/typography.d.ts
@@ -0,0 +1,77 @@
+/**
+ * This file is filled with components that are meant to wrap others as a
+ * form of Higher-Order components.  They Essentially wrap other components
+ * in order to add a class to the wrapped component.
+ *
+ * This file defines convenience wrappers for the various text formatting
+ * styles that are defined within the main.css files.  The primary reason
+ * for creating these higher-order components is to allow for them to be
+ * added to other components.
+ */
+import React from 'react';
+import { HigherOrderComponentWithClassNameProps } from '../higher_order';
+/**
+ * WithParagraphText100 is a function that takes a component and adds the
+ * class 'type--paragraph--text-100' to its className.
+ */
+export declare function WithParagraphText100<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithParagraphText300 is a function that takes a component and adds the
+ * class 'type--paragraph--text-300' to its className.
+ */
+export declare function WithParagraphText300<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithParagraphText500 is a function that takes a component and adds the
+ * class 'type--paragraph--text-500' to its className.
+ */
+export declare function WithParagraphText500<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithParagraphText600 is a function that takes a component and adds the
+ * class 'type--paragraph--text-600' to its className.
+ */
+export declare function WithParagraphText600<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithParagraphBase is a function that takes a component and adds the
+ * class 'type--paragraph--base' to its className.
+ */
+export declare function WithParagraphBase<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithParagraphSmall is a function that takes a component and adds the
+ * class 'type--paragraph--small' to its className.
+ */
+export declare function WithParagraphSmall<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiText100 is a function that takes a component and adds the
+ * class 'type--ui--text-100' to its className.
+ */
+export declare function WithUiText100<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiText300 is a function that takes a component and adds the
+ * class 'type--ui--text-300' to its className.
+ */
+export declare function WithUiText300<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiText500 is a function that takes a component and adds the
+ * class 'type--ui--text-500' to its className.
+ */
+export declare function WithUiText500<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiText600 is a function that takes a component and adds the
+ * class 'type--ui--text-600' to its className.
+ */
+export declare function WithUiText600<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiBase is a function that takes a component and adds the
+ * class 'type--ui--base' to its className.
+ */
+export declare function WithUiBase<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiSmall is a function that takes a component and adds the
+ * class 'type--ui--small' to its className.
+ */
+export declare function WithUiSmall<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
+/**
+ * WithUiButton is a function that takes a component and adds the
+ * class 'type--ui--button' to its className.
+ */
+export declare function WithUiButton<Props extends HigherOrderComponentWithClassNameProps>(component: React.ComponentType<Props> | string): React.FC<Props>;
