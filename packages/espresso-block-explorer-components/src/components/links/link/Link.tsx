da083d5708c687595b767f6e957afee48d452240
Theodore Schnepper <tschnepper@espressosys.com>
2024-01-12 13:55:38 -0500

    Add Link component for Anchor element

---
+++ packages/espresso-block-explorer-components/src/components/links/link/Link.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { addClassToClassName } from '../../higher_order';
+
+import './link.css';
+
+export interface LinkProps
+  extends React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {}
+
+/**
+ * Link component represents a simple Anchor tag link.  This component
+ * may be expanded and have many different variations.
+ */
+const Link: React.FC<LinkProps> = (props) => (
+  <a {...props} className={addClassToClassName(props.className, 'link')}>
+    {props.children}
+  </a>
+);
+
+export default Link;
---
+++ packages/espresso-block-explorer-components/src/components/links/link/__test__/Link.test.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { describe, expect, it } from 'vitest';
+import Text from '../../../text/Text';
+import Link from '../Link';
+
+describe('Link Component', () => {
+  it('should be in the document', () => {
+    render(
+      <Link data-testid="1" href="https://example.com/">
+        <Text text="Click Me!" />
+      </Link>
+    );
+
+    const link = screen.getByTestId('1');
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveClass('link');
+    expect(link).toHaveAttribute('href', 'https://example.com/');
+    expect(link).toHaveTextContent('Click Me!');
+  });
+});
