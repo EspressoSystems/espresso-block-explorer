b99269867f30ab93c067999099a53aca8ec98449
Theodore Schnepper <tschnepper@espressosys.com>
2024-01-24 07:43:33 -0700

    Add test case for Data Table sorting

--- packages/espresso-block-explorer-components/src/components/data/data_table/__test__/DataTable.test.tsx
+++ packages/espresso-block-explorer-components/src/components/data/data_table/__test__/DataTable.test.tsx
@@ -1,13 +1,13 @@
 import { composeStories } from '@storybook/react';
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { describe, expect, it } from 'vitest';
 import * as stories from '../__docs__/DataTable.stories';
 
 const { DataTable } = composeStories(stories);
 
-describe('DataTable Component', () => {
+describe('DataTable Component', async () => {
   it('should have a link specified', async () => {
     render(<DataTable data-testid="1" />);
 
@@ -20,5 +20,23 @@ describe('DataTable Component', () => {
 
     const dataTable = screen.getByTestId('1');
     expect(dataTable).toBeInTheDocument();
+    expect(dataTable).toHaveTextContent(
+      'OneTwo1one2two3three4four5five6six7seven8eight9nine10ten',
+    );
+
+    const column = screen.getByText('One');
+    fireEvent.click(column);
+
+    await waitFor(() => {
+      const ele = screen.getByTestId('1');
+      const tbody = ele.children[1];
+
+      expect(tbody.children.length).toBeGreaterThan(0);
+    });
+
+    expect(dataTable).toBeInTheDocument();
+    expect(dataTable).toHaveTextContent(
+      'OneTwo10ten9nine8eight7seven6six5five4four3three2two1one',
+    );
   });
 });
