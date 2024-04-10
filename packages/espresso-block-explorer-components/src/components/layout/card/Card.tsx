80435af7ad752df51ecc8b21417a4211bc96183f
Theodore Schnepper <tschnepper@espressosys.com>
2024-04-10 10:44:09 +0300

    Separate padding from Card component
    The padding on the Card component is convenient and is used in the
    majority of use cases.  However, there are a handful of cases where this
    padding is not wanted and hinders the design.  As such it is beneficial
    to separate the padding from the Card itself.  However, it is important
    to maintain the existing default behavior for consumers of the
    component.  As such, it is desirable to provide a separate component
    for the card that removes the padding instead of having it attached.
    
    This change separates the card padding from the card with separate
    classes, while also preserving the previous default padding behavior.

--- packages/espresso-block-explorer-components/src/components/layout/card/Card.tsx
+++ packages/espresso-block-explorer-components/src/components/layout/card/Card.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import './card.css';
 import {
   HigherOrderComponentWithClassNameProps,
   addClassToClassName,
 } from '../../higher_order';
+import './card.css';
 
 export interface CardProps extends HigherOrderComponentWithClassNameProps {}
 
+/**
+ * Card represents a component that is styled after a card style.  It is used
+ * as a general container that wants to have its contents displayed within a
+ * card style.
+ *
+ * The component is influenced by the card style from Material Design but is
+ * ultimately based on the Espresso Block Explorer Design.
+ *
+ * By default the card will have some default padding applied to it, which is
+ * there for convenience.  However there are cases where this initial padding
+ * is not desired.  In this case you should use the `CardNoPadding` component
+ * provided by this module as well.
+ */
 const Card: React.FC<CardProps> = (props) => (
+  <div
+    {...props}
+    className={addClassToClassName(props.className, 'card card--padding')}
+  />
+);
+
+/**
+ * CardNoPadding as a convenience component for creating a card that does not
+ * have any padding applied to it.  This is useful when you want to have a card
+ * that is flush with the edges of the container.  It allows one to style their
+ * card container, and customize other aspects of it to their liking.
+ */
+export const CardNoPadding: React.FC<CardProps> = (props) => (
   <div {...props} className={addClassToClassName(props.className, 'card')} />
 );
 
