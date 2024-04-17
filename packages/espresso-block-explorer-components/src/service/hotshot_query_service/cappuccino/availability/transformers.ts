06d0d7321401c8a98c1589b2d9622a00e7779e55
Theodore Schnepper <tschnepper@espressosys.com>
2024-04-17 06:55:27 -0600

    Add comments to conversion functions
    The conversion functions that act as helper functions to provide a way
    to translate between retrievable availability API types to desired derived
    data types are not documented.  Though what they are doing may be
    fairly straight forward, it helps to provide context as to why they are
    necessary in the first place.
    
    This adds comments to these functions in order to indicate what it is
    they are doing, why, and why they may no longer be necessary.

--- packages/espresso-block-explorer-components/src/service/hotshot_query_service/cappuccino/availability/transformers.ts
+++ packages/espresso-block-explorer-components/src/service/hotshot_query_service/cappuccino/availability/transformers.ts
@@ -8,6 +8,19 @@ import { CappuccinoDerivedTransactionSummary } from './derived_transaction_summa
 import { CappuccinoAPILeafResponse } from './leaf_response';
 import { CappuccinoAPITransactionResponse } from './transaction_response';
 
+/**
+ * convertCappuccinoBlockAndLeafToBlockSummary is a helper function that is able
+ * to convert ad CappuccinoAPIBlock and a CappuccinoAPILeafResponse into a
+ * CappuccinoDerivedBlockSummary.
+ *
+ * All of the data needed for the CappuccinoDerivedBlockSummary is present in
+ * the block and leaf, so this function is able to take the pieces it needs
+ * from either, and combine them to create the summary.  This is only necessary
+ * when the API is unable to provide this type directly.
+ *
+ * With the creation of the explorer API, this function should no longer be
+ * necessary.
+ */
 export async function convertCappuccinoBlockAndLeafToBlockSummary(
   block: CappuccinoAPIBlock,
   leaf: CappuccinoAPILeafResponse,
@@ -21,6 +34,17 @@ export async function convertCappuccinoBlockAndLeafToBlockSummary(
   );
 }
 
+/**
+ * convertCappuccinoBlockToBlockSummary is a helper function that is able to
+ * convert a CappuccinoAPIBlock into a CappuccinoDerivedBlockSummary.
+ *
+ * All of the data needed for the CappuccinoDerivedBlockSummary is present in
+ * the block, The block summary is just a different representation of the
+ * same data.
+ *
+ * With the creation of the explorer API, this function should no longer be
+ * necessary.
+ */
 export async function convertCappuccinoBlockToBlockSummary(
   block: CappuccinoAPIBlock,
 ): Promise<CappuccinoDerivedBlockSummary> {
@@ -33,6 +57,22 @@ export async function convertCappuccinoBlockToBlockSummary(
   );
 }
 
+/**
+ * convertCappuccinoLeafAndTransactionsToTransactionSummaries is a helper
+ * function that is able to convert a CappuccinoAPILeafResponse and an
+ * AsyncIterable of CappuccinoAPITransactionResponse into an AsyncIterable of
+ * CappuccinoDerivedTransactionSummary.
+ *
+ * The leaf is needed to provide the block header, as well as the namespace
+ * that corresponds to the transactions.  The transactions are needed to provide
+ * the transaction data.
+ *
+ * This function is only necessary when the API is unable to provide this type
+ * directly.
+ *
+ * With the creation of the explorer API, this function should no longer be
+ * necessary.
+ */
 export async function* convertCappuccinoLeafAndTransactionsToTransactionSummaries(
   leaf: CappuccinoAPILeafResponse,
   transactions: AsyncIterable<CappuccinoAPITransactionResponse>,
