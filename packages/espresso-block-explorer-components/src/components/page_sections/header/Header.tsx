67d6b8b69aaf9bf3cbcdbf3a3b9b672c939594f6
Rob <rob@espressosys.com>
2024-05-20 13:36:06 -0400

    update link to use path resolver

--- packages/espresso-block-explorer-components/src/components/page_sections/header/Header.tsx
+++ packages/espresso-block-explorer-components/src/components/page_sections/header/Header.tsx
@@ -2,7 +2,7 @@ import EspressoLogoAndTitle from '@/visual/icons/EspressoLogoAndTitle';
 import React from 'react';
 import NavBar from '../../links/nav_bar/NavBar';
 import './header.css';
-import { Link } from '../..';
+import { Link, PathResolverContext } from '../..';
 
 interface HeaderProps { }
 
@@ -10,14 +10,18 @@ interface HeaderProps { }
  * Header is a component for creating and display a consistent Header for
  * every Block Explorer Page.
  */
-const Header: React.FC<HeaderProps> = (props) => (
-  <header {...props}>
-    <Link href="/">
-      <EspressoLogoAndTitle />
-    </Link>
-    {/* Spacer */}
-    <NavBar />
-  </header>
-);
+const Header: React.FC<HeaderProps> = (props) => {
+  const resolver = React.useContext(PathResolverContext);
+
+  return (
+    <header {...props}>
+      <Link href={resolver.explorer()}>
+        <EspressoLogoAndTitle />
+      </Link>
+      {/* Spacer */}
+      <NavBar />
+    </header>
+  )
+};
 
 export default Header;
