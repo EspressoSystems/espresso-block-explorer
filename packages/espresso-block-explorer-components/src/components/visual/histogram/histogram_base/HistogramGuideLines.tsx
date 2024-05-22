79f6c804f6522425f9542319ca9ea695ed836919
Theodore Schnepper <tschnepper@espressosys.com>
2024-05-22 07:54:47 -0600

    Fix Histogram missing y axis labels
    When a histogram is displaying values that are all less than 1, then the
    resulting y axis labels only has a single value displayed.  This makes it
    difficult to determine what the values on the histogram actually are.  The
    reason for this is that we're exclusively picking whole numbers on the
    y-axis guidelines.
    
    To fix this issue this removes the exclusive use of whole numbers for
    y-axis labels.

--- packages/espresso-block-explorer-components/src/components/visual/histogram/histogram_base/HistogramGuideLines.tsx
+++ packages/espresso-block-explorer-components/src/components/visual/histogram/histogram_base/HistogramGuideLines.tsx
@@ -38,14 +38,9 @@ export const ProvideGuideLines: React.FC<ProvideGuideLinesProps> = ({
   const desiredGuideLineCount = 4;
 
   const lines: number[] = [];
-  const step = Math.max(
-    Math.round(
-      (rangeStatistics.max - rangeStatistics.min) / desiredGuideLineCount,
-    ),
-    1,
-  );
-
-  for (let i = 0; i < rangeAffineTransform.inputMax && step > 0; i += step) {
+  const step =
+    (rangeStatistics.max - rangeStatistics.min) / desiredGuideLineCount;
+  for (let i = 0; i <= rangeAffineTransform.inputMax && step > 0; i += step) {
     lines.push(i);
   }
 
