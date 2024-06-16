90938b6e159edbccdded420fbcf2572b0fad3faf
Theodore Schnepper <tschnepper@espressosys.com>
2024-06-16 10:36:24 -0600

    Add first and last interactions with iterators / iterables
    Add empty and unimplemented AsyncIterableIterators for testing /
    convenience.

--- packages/espresso-block-explorer-components/src/functional/functional.ts
+++ packages/espresso-block-explorer-components/src/functional/functional.ts
@@ -179,6 +179,39 @@ export function firstIterator<T>(iterator: Iterator<T>): T {
 }
 
 /**
+ * firstIterable is a convenience function for invoking firstIterator with
+ * an Iterable instead.
+ */
+export function firstIterable<T>(iterable: Iterable<T>): T {
+  return firstIterator(iterable[Symbol.iterator]());
+}
+
+/**
+ * lastIterator returns the last element of the given Iterator
+ * If no element is found, this throws an error.
+ */
+export function lastIterator<T>(iterator: Iterator<T>): T {
+  let last: undefined | T = undefined;
+  for (let next = iterator.next(); !next.done; next = iterator.next()) {
+    last = next.value;
+  }
+
+  if (last === undefined) {
+    throw new MissingElementError();
+  }
+
+  return last;
+}
+
+/**
+ * lastIterable is a convenience function for invoking lastIterator with
+ * an Iterable instead.
+ */
+export function lastIterable<T>(iterable: Iterable<T>): T {
+  return lastIterator(iterable[Symbol.iterator]());
+}
+
+/**
  * firstWhereIterator is a find function, but operating on Javascript iterators.
  */
 export function firstWhereIterator<T, S extends T>(
