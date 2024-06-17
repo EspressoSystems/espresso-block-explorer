df23e8cb97bb7fc78c266b61f15c2348c423aad2
Theodore Schnepper <tschnepper@espressosys.com>
2024-06-17 07:57:12 -0600

    Feat expand abbreviated country names in countries pie chart

--- packages/espresso-block-explorer-components/src/components/page_sections/countries_pie_chart/CountriesPieChart.tsx
+++ packages/espresso-block-explorer-components/src/components/page_sections/countries_pie_chart/CountriesPieChart.tsx
@@ -1,24 +1,25 @@
-import NumberText from '@/components/text/NumberText';
 import {
   PieChart,
   PieChartEntry,
-  PieChartLabelContext,
-  PieChartLabelProps,
 } from '@/components/visual/pie_chart/PieChart';
 import { PieChartSectionTitle } from '@/components/visual/pie_chart/pie_chart_section_title/PieChartSectionTitle';
 import { DataContext } from '@/contexts/DataProvider';
 import { LoadingContext } from '@/contexts/LoadingProvider';
 import Text from '@/text/Text';
 import React from 'react';
+import { alpha2CountryMap } from './alpha2CountryMap';
 
-const LabelValue: React.FC<PieChartLabelProps> = (props) => {
-  return (
-    <>
-      <Text text={props.label} />
-      <NumberText number={props.value} />
-    </>
-  );
-};
+function expandAbbreviatedCountryName(abbreviatedCountryName: string): string {
+  const value = alpha2CountryMap[abbreviatedCountryName];
+  return value ?? abbreviatedCountryName;
+}
+
+function expandPieChartEntry(entry: PieChartEntry): PieChartEntry {
+  return {
+    label: expandAbbreviatedCountryName(entry.label),
+    value: entry.value,
+  };
+}
 
 export const CountriesPieChart: React.FC = () => {
   const loading = React.useContext(LoadingContext);
@@ -29,12 +30,12 @@ export const CountriesPieChart: React.FC = () => {
   }
 
   return (
-    <PieChartLabelContext.Provider value={LabelValue}>
+    <>
       <PieChartSectionTitle>
         <Text text="Countries" />
       </PieChartSectionTitle>
 
-      <PieChart values={histogramData} />
-    </PieChartLabelContext.Provider>
+      <PieChart values={histogramData.map(expandPieChartEntry)} />
+    </>
   );
 };
