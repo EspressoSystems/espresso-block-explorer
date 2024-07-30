67a9f2b29d6f0a1601dd12b2bf054b3b6f5465ed
Theodore Schnepper <tschnepper@espressosys.com>
2024-07-30 10:28:19 -0600

    Upgrade to Storybook 8

--- packages/espresso-block-explorer-components/.storybook/main.ts
+++ packages/espresso-block-explorer-components/.storybook/main.ts
@@ -11,18 +11,23 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     getAbsolutePath('@storybook/addon-a11y'),
   ],
+
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
