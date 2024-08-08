46d6f1288b444c56c3fa3d8fc48f365f27590d06
Ayiga <Ayiga@users.noreply.github.com>
2024-08-08 13:28:12 +0000

    deploy: 7b843d20e86aa8efb4b693c61accd2709cebb12b

---
+++ src/components/page_sections/cdn_status/CDNStatus.d.ts
@@ -0,0 +1,13 @@
+import { default as React } from '../../../../../../node_modules/react';
+
+interface CDNStatusProps {
+    className?: string;
+}
+/**
+ * CDNStatus is a component that represents the CDN's current status.
+ * At the moment, we have no way of retrieving this information, or seeing
+ * the current status of the CDN.  So for now this always just reports the
+ * CDN as being online.
+ */
+export declare const CDNStatus: React.FC<CDNStatusProps>;
+export {};
