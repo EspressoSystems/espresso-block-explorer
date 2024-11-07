0bb01f2a1d228b0937bc8bda6ed4437f1ed7c7fd
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-06 19:51:27 -0700

    Remove bad exports

--- packages/espresso-block-explorer-components/src/service/inscription/cappuccino/requests/index.ts
+++ packages/espresso-block-explorer-components/src/service/inscription/cappuccino/requests/index.ts
@@ -1,6 +1,2 @@
 export * from './inscription_request';
 export * from './inscription_request_codec';
-export * from './web_worker_life_cycle_request';
-export * from './web_worker_life_cycle_request_codec';
-export * from './web_worker_proxy_request';
-export * from './web_worker_proxy_request_codec';
