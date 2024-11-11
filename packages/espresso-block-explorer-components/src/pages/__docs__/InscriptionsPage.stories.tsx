b9c34143eb82407275ca807931e433ffbbb634b9
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-11 16:37:49 -0700

    Fix missing changes from InscriptionsPage

--- packages/espresso-block-explorer-components/src/pages/__docs__/InscriptionsPage.stories.tsx
+++ packages/espresso-block-explorer-components/src/pages/__docs__/InscriptionsPage.stories.tsx
@@ -9,25 +9,52 @@ import InscriptionsPage from '../InscriptionsPage';
 
 interface ExampleProps {}
 
-const Example: React.FC<ExampleProps> = ({ ...props }) => (
-  <div style={{ '--font-family--ibm-plex-mono': "'IBM Plex Mono', serif" }}>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
-      rel="stylesheet"
+const Example: React.FC<ExampleProps> = ({ ...props }) => {
+  const backgroundImage = (
+    <img
+      src="/infinite_garden@1x.jpg"
+      srcSet="/infinite_garden@2x.jpg 2x, /infinite_garden @3x"
+      alt="The Infinite Garden"
     />
-    <ProvideTickEverySecond>
-      <OverridePathResolver pathResolver={new StoryBookPathResolver()}>
-        <ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
-          <ProvideCappuccinoInscriptionStreams>
-            <InscriptionsPage {...props} />
-          </ProvideCappuccinoInscriptionStreams>
-        </ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
-      </OverridePathResolver>
-    </ProvideTickEverySecond>
-  </div>
-);
+  );
+
+  const escapeTheWalledGardensImage = (
+    <img
+      src="/escape_the_walled_gardens@1x.jpg"
+      srcSet="/escape_the_walled_gardens@2x.jpg 2x, /escape_the_walled_gardens@3x.jpg 3x"
+      alt="Escape the Walled Gardens"
+    />
+  );
+
+  return (
+    <div
+      style={{
+        '--font-family--ibm-plex-mono': "'IBM Plex Mono', serif",
+        '--font-family--neue-montreal': "'PP Neue Montreal', sans-serif",
+      }}
+    >
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
+        rel="stylesheet"
+      />
+      <ProvideTickEverySecond>
+        <OverridePathResolver pathResolver={new StoryBookPathResolver()}>
+          <ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
+            <ProvideCappuccinoInscriptionStreams>
+              <InscriptionsPage
+                backgroundImage={backgroundImage}
+                escapeTheWalledGardensImage={escapeTheWalledGardensImage}
+                {...props}
+              />
+            </ProvideCappuccinoInscriptionStreams>
+          </ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
+        </OverridePathResolver>
+      </ProvideTickEverySecond>
+    </div>
+  );
+};
 
 const meta: Meta = {
   title: 'Pages/Inscriptions',
