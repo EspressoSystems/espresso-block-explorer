b7601bc0347f4dc2d04b0c2d05960c125e9c728c
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-11 16:39:40 -0700

    Replace images on inscriptions

--- packages/inscriptions/src/app/home_suspended.tsx
+++ packages/inscriptions/src/app/home_suspended.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import Image from 'next/image';
+import walledGardenPng from './escape_the_walled_gardens@1x.jpg';
+import infiniteGardenPng from './infinite_garden@1x.jpg';
+
 import {
   InscriptionsPage,
   ProvideCappuccinoInscriptionStreams,
@@ -7,10 +11,19 @@ import {
 } from 'espresso-block-explorer-components';
 
 export default function HomeSuspended() {
+  const backgroundImage = (
+    <Image src={infiniteGardenPng} alt="Infinite Garden" />
+  );
+  const escapeTheWalledGardenImage = (
+    <Image src={walledGardenPng} alt="Escape the Walled Gardens" />
+  );
   return (
     <ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
       <ProvideCappuccinoInscriptionStreams>
-        <InscriptionsPage />
+        <InscriptionsPage
+          backgroundImage={backgroundImage}
+          escapeTheWalledGardensImage={escapeTheWalledGardenImage}
+        />
       </ProvideCappuccinoInscriptionStreams>
     </ProvideWebWorkerCappuccinoInscriptionServiceAPIContext>
   );
