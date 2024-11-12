0230e709ee667177443732ff0443f428ddc1013d
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-12 09:53:20 -0700

    Adjust style type to remove error

--- packages/espresso-block-explorer-components/src/pages/__docs__/InscriptionsPage.stories.tsx
+++ packages/espresso-block-explorer-components/src/pages/__docs__/InscriptionsPage.stories.tsx
@@ -3,7 +3,7 @@ import { OverridePathResolver } from '@/components/contexts/PathResolverProvider
 import { ProvideCappuccinoInscriptionStreams } from 'pages/CappuccinoInscriptionServiceAdapters';
 import { ProvideWebWorkerCappuccinoInscriptionServiceAPIContext } from 'pages/CappuccinoInscriptionServiceAPIContext';
 import { StoryBookPathResolver } from 'pages/StoryBookPathResolver';
-import React from 'react';
+import React, { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { Meta, StoryObj } from 'storybook';
 import InscriptionsPage from '../InscriptionsPage';
 
@@ -28,10 +28,12 @@ const Example: React.FC<ExampleProps> = ({ ...props }) => {
 
   return (
     <div
-      style={{
-        '--font-family--ibm-plex-mono': "'IBM Plex Mono', serif",
-        '--font-family--neue-montreal': "'PP Neue Montreal', sans-serif",
-      }}
+      style={
+        {
+          '--font-family--ibm-plex-mono': "'IBM Plex Mono', serif",
+          '--font-family--neue-montreal': "'PP Neue Montreal', sans-serif",
+        } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+      }
     >
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
