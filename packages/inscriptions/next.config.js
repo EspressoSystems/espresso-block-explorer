dc770b8d95f58920e9cf2cd97600dd27f1dff4a3
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-12 10:33:39 -0700

    Adjust next config for inscriptions

--- packages/inscriptions/next.config.js
+++ packages/inscriptions/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.optimization.splitChunks = false;
+    }
 
-module.exports = nextConfig
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
