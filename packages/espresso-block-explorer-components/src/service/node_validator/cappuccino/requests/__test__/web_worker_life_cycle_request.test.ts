ce961af34972728b2fb0cede0aee303ba1746d4f
Theodore Schnepper <tschnepper@espressosys.com>
2024-11-22 14:48:13 -0700

    Fix tests and errors

--- packages/espresso-block-explorer-components/src/service/node_validator/cappuccino/requests/__test__/web_worker_life_cycle_request.test.ts
+++ packages/espresso-block-explorer-components/src/service/node_validator/cappuccino/requests/__test__/web_worker_life_cycle_request.test.ts
@@ -1,26 +1,35 @@
 import { zipWithIterable } from '@/functional/functional';
+import {
+  kWebSocketCommandCloseType,
+  WebSocketCommandClose,
+} from '@/models/web_worker/web_socket/request/close';
+import {
+  kWebSocketCommandConnectType,
+  WebSocketCommandConnect,
+} from '@/models/web_worker/web_socket/request/connect';
+import { webSocketCommandCodec } from '@/models/web_worker/web_socket/request/web_socket_command_codec';
 import { describe, it } from 'vitest';
-import { Close, Connect } from '../web_worker_life_cycle_request';
-import { webWorkerLifeCycleRequestCodec } from '../web_worker_life_cycle_request_codec';
 
 describe('WebWorkerLifeCycleRequestCodec', () => {
   it('should JSON encode', () => {
-    const requestValues = ['Connect', 'Close'];
+    const requestValues = [
+      kWebSocketCommandConnectType,
+      kWebSocketCommandCloseType,
+    ];
 
-    const requestConstructors = [Connect, Close];
+    const requestConstructors = [
+      WebSocketCommandConnect,
+      WebSocketCommandClose,
+    ];
 
     for (const [string, constructor] of zipWithIterable(
       requestValues,
       requestConstructors,
       (a, b) => [a, b] as const,
     )) {
-      expect(webWorkerLifeCycleRequestCodec.encode(new constructor())).toBe(
-        string,
-      );
+      expect(webSocketCommandCodec.encode(new constructor())).toBe(string);
 
-      expect(webWorkerLifeCycleRequestCodec.decode(string)).toBeInstanceOf(
-        constructor,
-      );
+      expect(webSocketCommandCodec.decode(string)).toBeInstanceOf(constructor);
 
       expect(new constructor().valueOf()).toBe(string);
     }
