5710804ce0b1dae8b1cf82cee1f37ae865aa5db9
Theodore Schnepper <tschnepper@espressosys.com>
2024-12-11 11:24:55 -0700

    Add further checks to the NodesPage smoke test
    The NodesPage smoke test doesn't currently test any of the functionality and
    rendering.  Instead it just checks that the initial render works, and nothing
    else matters.  This test should check to ensure that the various card sections
    load and display information as expected.
    
    This change adds extra checks to the NodePage smoke test to ensure that
    the data is being processed, loaded, and that the components are being
    displayed without issue.

--- packages/espresso-block-explorer-components/src/pages/__test__/NodesPage.test.tsx
+++ packages/espresso-block-explorer-components/src/pages/__test__/NodesPage.test.tsx
@@ -8,17 +8,247 @@ const { Nodes } = composeStories(stories);
 
 window.HTMLElement.prototype.scrollIntoView = function () {};
 
-describe('TransactionsForBlockPage', async () => {
+describe('NodesPage', async () => {
   it('should render the story', async () => {
     render(<Nodes data-testid="1" />);
 
+    // Wait for the main page to be present.
     await waitFor(() => {
       const ele = screen.getByTestId('1');
-      const elements = ele.querySelectorAll('td');
-      expect(elements.length).greaterThan(0);
+      expect(ele).toBeInTheDocument();
     });
 
-    const blockPage = screen.getByTestId('1');
-    expect(blockPage).toBeInTheDocument();
+    const nodePage = screen.getByTestId('1');
+    expect(nodePage).toBeInTheDocument();
+
+    // Wait for the cards to all be rendered
+    await waitFor(
+      () => {
+        const cards = nodePage.querySelectorAll('.card');
+        // we should have 12 card sections
+        expect(cards).toHaveLength(12);
+      },
+      { container: nodePage },
+    );
+
+    // Latest Block Section
+    {
+      const latestBlockSection: HTMLElement | null =
+        nodePage.querySelector('.latest-block.card');
+      expect(latestBlockSection).not.toBeNull();
+      if (!latestBlockSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait until we have row data in the latest block table
+      await waitFor(
+        () => {
+          const summaryValues = latestBlockSection.querySelectorAll(
+            '.summary-tabled-labeled-value',
+          );
+          expect(summaryValues).toHaveLength(5);
+        },
+        { container: latestBlockSection },
+      );
+    }
+
+    // Latest Block Producers Section
+    {
+      const latestBlockProducersSection: HTMLElement | null =
+        nodePage.querySelector('.latest-block-producers.card');
+      expect(latestBlockProducersSection).not.toBeNull();
+      if (!latestBlockProducersSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait until we have row data in the latest block producers table
+      await waitFor(
+        () => {
+          const rows = latestBlockProducersSection.querySelectorAll(
+            '.summary-tabled-labeled-value',
+          );
+          expect(rows.length).toBeGreaterThan(0);
+        },
+        { container: latestBlockProducersSection },
+      );
+    }
+
+    // Block Time Histogram Section
+    {
+      const blockTimeHistogramSection: HTMLElement | null =
+        nodePage.querySelector('.block-time-histogram.card');
+      expect(blockTimeHistogramSection).not.toBeNull();
+      if (!blockTimeHistogramSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait for histogram plot graphics datagroup
+      await waitFor(
+        () => {
+          const histogramPlotGraphics: HTMLElement | null =
+            blockTimeHistogramSection.querySelector(
+              '.histogram-plot[role="graphics-datagroup"]',
+            );
+          expect(histogramPlotGraphics).not.toBeNull();
+        },
+        { container: blockTimeHistogramSection },
+      );
+
+      const histogramPlotGraphics: HTMLElement | null =
+        blockTimeHistogramSection.querySelector(
+          '.histogram-plot[role="graphics-datagroup"]',
+        );
+
+      if (!histogramPlotGraphics) {
+        return;
+      }
+
+      // Wait until we have row data in the block time graph
+      await waitFor(
+        () => {
+          expect(histogramPlotGraphics.children).toHaveLength(50);
+        },
+        { container: histogramPlotGraphics },
+      );
+    }
+
+    // Block Size Histogram Section
+    {
+      const blockSizeHistogramSection: HTMLElement | null =
+        nodePage.querySelector('.block-size-histogram.card');
+      expect(blockSizeHistogramSection).not.toBeNull();
+      if (!blockSizeHistogramSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait for histogram plot graphics datagroup
+      await waitFor(
+        () => {
+          const histogramPlotGraphics: HTMLElement | null =
+            blockSizeHistogramSection.querySelector(
+              '.histogram-plot[role="graphics-datagroup"]',
+            );
+          expect(histogramPlotGraphics).not.toBeNull();
+        },
+        { container: blockSizeHistogramSection },
+      );
+
+      const histogramPlotGraphics: HTMLElement | null =
+        blockSizeHistogramSection.querySelector(
+          '.histogram-plot[role="graphics-datagroup"]',
+        );
+
+      if (!histogramPlotGraphics) {
+        return;
+      }
+
+      // Wait until we have row data in the block time graph
+      await waitFor(
+        () => {
+          expect(histogramPlotGraphics.children).toHaveLength(50);
+        },
+        { container: histogramPlotGraphics },
+      );
+    }
+
+    // Throughput Histogram Section
+    {
+      const throughputHistogramSection: HTMLElement | null =
+        nodePage.querySelector('.throughput-histogram.card');
+      expect(throughputHistogramSection).not.toBeNull();
+      if (!throughputHistogramSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait for histogram plot graphics datagroup
+      await waitFor(
+        () => {
+          const histogramPlotGraphics: HTMLElement | null =
+            throughputHistogramSection.querySelector(
+              '.histogram-plot[role="graphics-datagroup"]',
+            );
+          expect(histogramPlotGraphics).not.toBeNull();
+        },
+        { container: throughputHistogramSection },
+      );
+
+      const histogramPlotGraphics: HTMLElement | null =
+        throughputHistogramSection.querySelector(
+          '.histogram-plot[role="graphics-datagroup"]',
+        );
+
+      if (!histogramPlotGraphics) {
+        return;
+      }
+
+      // Wait until we have row data in the block time graph
+      await waitFor(
+        () => {
+          expect(histogramPlotGraphics.children).toHaveLength(50);
+        },
+        { container: histogramPlotGraphics },
+      );
+    }
+
+    // Network Map Section
+    {
+      const networkMapSection: HTMLElement | null =
+        nodePage.querySelector('.network-map.card');
+      expect(networkMapSection).not.toBeNull();
+      if (!networkMapSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait until we have row data in the network map
+      await waitFor(
+        () => {
+          const worldMapPopulationDots = networkMapSection.querySelector(
+            '.world-map-population-dots',
+          );
+          expect(worldMapPopulationDots).not.toBeNull();
+          if (!worldMapPopulationDots) {
+            return;
+          }
+          expect(worldMapPopulationDots).toBeInTheDocument();
+          expect(worldMapPopulationDots.children.length).toBeGreaterThan(0);
+        },
+        { container: networkMapSection },
+      );
+    }
+
+    // Nodes Table Section
+    {
+      const nodesSection: HTMLElement | null =
+        nodePage.querySelector('.nodes.card');
+      expect(nodesSection).not.toBeNull();
+      if (!nodesSection) {
+        // This is here just to satisfy typescript. In reality, this should
+        // be unreachable.
+        return;
+      }
+
+      // Wait until we have row data in the nodes table
+      await waitFor(
+        () => {
+          const rows = nodesSection.querySelectorAll('tr');
+          expect(rows.length).toBeGreaterThan(10);
+        },
+        { container: nodesSection },
+      );
+    }
+
+    expect(nodePage).toBeInTheDocument();
   });
 });
