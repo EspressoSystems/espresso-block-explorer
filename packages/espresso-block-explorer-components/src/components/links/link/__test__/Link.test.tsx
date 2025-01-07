345ed8d1f60abec694a8f260d255fb62328451c3
Theodore Schnepper <tschnepper@espressosys.com>
2025-01-07 07:54:40 -0700

    Fix Link test
    The Link test wasn't changed from the previous version referencing the
    now non-existent Link component.  The test has been modified to test
    the two separate components instead.
    
    Additionally, a test has been added to check that the InternalLink
    can have it's component replaced by utilizing the
    `InternalLinkAnchorComponentContext` context.

--- packages/espresso-block-explorer-components/src/components/links/link/__test__/Link.test.tsx
+++ packages/espresso-block-explorer-components/src/components/links/link/__test__/Link.test.tsx
@@ -1,15 +1,20 @@
 import Text from '@/text/Text';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import { describe, expect, it } from 'vitest';
-import Link from '../Link';
+import {
+  EgressLink,
+  InternalLink,
+  InternalLinkAnchorComponentContext,
+} from '../Link';
 
-describe('Link Component', () => {
+describe('EgressLink Component', () => {
   it('should be in the document', () => {
     render(
-      <Link data-testid="1" href="https://example.com/">
+      <EgressLink data-testid="1" href="https://example.com/">
         <Text text="Click Me!" />
-      </Link>,
+      </EgressLink>,
     );
 
     const link = screen.getByTestId('1');
@@ -17,5 +22,49 @@ describe('Link Component', () => {
     expect(link).toHaveClass('link');
     expect(link).toHaveAttribute('href', 'https://example.com/');
     expect(link).toHaveTextContent('Click Me!');
+  });
+});
+
+describe('InternalLink Component', () => {
+  describe('Default anchor component', () => {
+    it('should be in the document', () => {
+      render(
+        <InternalLink data-testid="1" href="https://example.com/">
+          <Text text="Click Me!" />
+        </InternalLink>,
+      );
+
+      const link = screen.getByTestId('1');
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveClass('link');
+      expect(link).toHaveAttribute('href', 'https://example.com/');
+      expect(link).toHaveTextContent('Click Me!');
+    });
+  });
+
+  describe('Replace component used', () => {
+    it('should be in the document', () => {
+      const CustomAnchor: React.FC<
+        React.AnchorHTMLAttributes<HTMLAnchorElement>
+      > = (props) => {
+        return <a {...props} data-custom={1} />;
+      };
+
+      render(
+        <InternalLinkAnchorComponentContext.Provider value={CustomAnchor}>
+          <InternalLink data-testid="1" href="https://example.com/">
+            <Text text="Click Me!" />
+          </InternalLink>
+          ,
+        </InternalLinkAnchorComponentContext.Provider>,
+      );
+
+      const link = screen.getByTestId('1');
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveClass('link');
+      expect(link).toHaveAttribute('href', 'https://example.com/');
+      expect(link).toHaveAttribute('data-custom', '1');
+      expect(link).toHaveTextContent('Click Me!');
+    });
   });
 });
