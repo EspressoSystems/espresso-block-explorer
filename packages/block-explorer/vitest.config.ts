65ce67dd25a3d39f381ae3a7bbc7bc2a467ad53e
Theodore Schnepper <tschnepper@espressosys.com>
2025-06-19 09:59:58 -0600

    Fix vitest configuration for block-explorer package
    vitest seems to be unable to resolve the typescript imports when running
    tests.  However, it is able to resolve them when building the project.
    Based on a search for the error, a stack overflow post indicated that the
    reason is that the tsconfig has defined the alias, but vitest still needs to
    be informed about it.  The solution is to use a plugin for tsconfig paths
    for vitest.

--- packages/block-explorer/vitest.config.ts
+++ packages/block-explorer/vitest.config.ts
@@ -2,10 +2,11 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   test: {
     globals: true,
     globalSetup: './vitest.global-setup.ts',
