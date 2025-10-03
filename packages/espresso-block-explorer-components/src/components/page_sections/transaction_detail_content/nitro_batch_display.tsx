5470a14e004d6255aed32c5c23d9a94569244585
Theodore Schnepper <tschnepper@espressosys.com>
2025-10-03 11:57:44 -0600

    Split arbitrum nitro specific code

--- packages/espresso-block-explorer-components/src/components/page_sections/transaction_detail_content/nitro_batch_display.tsx
+++ packages/espresso-block-explorer-components/src/components/page_sections/transaction_detail_content/nitro_batch_display.tsx
@@ -1,114 +1,21 @@
 import { assertNotNull } from '@/assert/assert';
 import { DateTimeText } from '@/components/text';
+import CopyWalletAddress from '@/components/text/CopyWalletAddress';
 import WalletAddressText from '@/components/text/WalletAddressText';
+import { uint8ArrayToArrayBufferCodec } from '@/convert/codec/uint8_array';
 import { createBufferedDataView, Endianess } from '@/convert/data_view';
 import { createRLPDeserializer } from '@/convert/rlp';
 import TableLabeledValue from '@/layout/table_labeled_value/TableLabeledValue';
 import { isNitroIntegrationNamespace } from '@/models/block_explorer/rollup_entry/data';
-import WalletAddress from '@/models/wallet_address/wallet_address';
+import {
+  decodeNitroL1IncomingMessage,
+  NitroL1IncomingMessage,
+} from '@/service/abritrum_nitro/l1_incoming_message';
 import NumberText from '@/text/NumberText';
 import Text from '@/text/Text';
 import React from 'react';
 import { TransactionDetailContext } from './TransactionDetailLoader';
 import { HexDumpAndCopyButtons } from './copy_as';
-
-/**
- * NitroL1IncomingMessageHeader represents the header of a Nitro L1
- * Incoming Message.
- *
- * This layout is adapted from the Nitro's code base:
- * https://github.com/EspressoSystems/nitro-espresso-integration/blob/828cc2a47358a8aac2a4931fe087f07037351164/arbos/arbostypes/incomingmessage.go#L38-L45
- */
-export interface NitroL1IncomingMessageHeader {
-  readonly kind: number;
-  readonly poster: Uint8Array;
-  readonly blockNumber: bigint;
-  readonly timestamp: bigint;
-  readonly requestID: null | Uint8Array;
-  readonly l1BaseFee: null | bigint;
-}
-
-/**
- * decodeNitroL1IncomingMessageHeader attempts to decode a Nitro L1
- * Incoming Message Header from the provided data.
- *
- * This data is RLP encoded.
- */
-export function decodeNitroL1IncomingMessageHeader(
-  data: Uint8Array,
-): null | NitroL1IncomingMessageHeader {
-  const deserializer = createRLPDeserializer(
-    createBufferedDataView(new Uint8Array(data).buffer, Endianess.big),
-  );
-
-  try {
-    const kind = deserializer.deserializeUint8();
-    const poster = deserializer.deserializeBytes();
-    const blockNumber = deserializer.deserializeUint64();
-    const timestamp = deserializer.deserializeUint64();
-    const requestID = deserializer.deserializeBytes();
-    const l1BaseFee = deserializer.deserializeUint64();
-
-    return {
-      kind,
-      poster,
-      blockNumber,
-      timestamp,
-      requestID: requestID.length > 0 ? requestID : null,
-      l1BaseFee: l1BaseFee > 0n ? l1BaseFee : null,
-    };
-  } catch (err) {
-    console.debug(
-      'failed to decode nitro l1 incoming message header due to error',
-      err,
-    );
-    return null;
-  }
-}
-
-/**
- * NitroL1IncomingMessage represents a Nitro L1 Incoming Message.
- *
- * This layout is adapted from the Nitro's code base:
- * https://github.com/EspressoSystems/nitro-espresso-integration/blob/828cc2a47358a8aac2a4931fe087f07037351164/arbos/arbostypes/incomingmessage.go#L58-L64
- */
-export interface NitroL1IncomingMessage {
-  readonly header: NitroL1IncomingMessageHeader;
-  readonly l2Msg: Uint8Array;
-  readonly batchGasCost: null | bigint;
-}
-
-/**
- * decodeNitroL1IncomingMessage attempts to decode a Nitro L1
- * Incoming Message from the provided data.
- *
- * This data is RLP encoded.
- */
-export function decodeNitroL1IncomingMessage(
-  data: Uint8Array,
-): null | NitroL1IncomingMessage {
-  const deserializer = createRLPDeserializer(
-    createBufferedDataView(new Uint8Array(data).buffer, Endianess.big),
-  );
-
-  try {
-    const headerData = deserializer.deserializeBytes();
-    const l2MessageData = deserializer.deserializeBytes();
-    // const batchGasCost = deserializer.deserializeUint64();
-
-    const header = decodeNitroL1IncomingMessageHeader(headerData);
-    assertNotNull(header);
-
-    return {
-      header,
-      l2Msg: l2MessageData,
-      batchGasCost: null,
-    };
-  } catch (err) {
-    console.error('Failed to decode Nitro L1 Incoming Message', err);
-    return null;
-  }
-}
 
 /**
  * NitroMessageWithMetadata represents a Nitro Message with metadata
@@ -117,9 +24,15 @@ export function decodeNitroL1IncomingMessage(
  * This layout is adapted from the Nitro's code base:
  * https://github.com/EspressoSystems/nitro-espresso-integration/blob/828cc2a47358a8aac2a4931fe087f07037351164/arbos/arbostypes/messagewithmeta.go#L17-L20
  */
-export interface NitroMessageWithMetadata {
+export class NitroMessageWithMetadata {
   readonly message: NitroL1IncomingMessage;
   readonly delayedMessagesRead: bigint;
+
+  constructor(message: NitroL1IncomingMessage, delayedMessagesRead: bigint) {
+    this.message = message;
+    this.delayedMessagesRead = delayedMessagesRead;
+    Object.freeze(this);
+  }
 }
 
 /**
@@ -150,10 +63,7 @@ export function decodeNitroMessageWithMetadata(
 
     const message = decodeNitroL1IncomingMessage(messageData);
     assertNotNull(message);
-    return {
-      message,
-      delayedMessagesRead,
-    };
+    return new NitroMessageWithMetadata(message, delayedMessagesRead);
   } catch (err) {
     console.debug('Failed to decode Nitro Message with Metadata', err);
     return null;
@@ -164,21 +74,40 @@ export function decodeNitroMessageWithMetadata(
  * batch submitted for Nitro integration projects that are submitted to
  * Espresso.
  */
-export interface NitroMessageV0 {
+export class NitroMessageV0 {
   readonly index: bigint;
   readonly length: bigint;
-  readonly contents: Uint8Array;
+  readonly contents: ArrayBuffer;
 
   readonly messageWithMetadata: null | NitroMessageWithMetadata;
+
+  constructor(
+    index: bigint,
+    length: bigint,
+    contents: ArrayBuffer,
+    messageWithMetadata: null | NitroMessageWithMetadata,
+  ) {
+    this.index = index;
+    this.length = length;
+    this.contents = contents;
+    this.messageWithMetadata = messageWithMetadata;
+    Object.freeze(this);
+  }
 }
 
 /**
  * NitroBatchV0 represents a Nitro Batch that is submitted to Espresso.
  * It contains a signature and a list of NitroMessages.
  */
-export interface NitroBatchV0 {
-  readonly signature: Uint8Array;
+export class NitroBatchV0 {
+  readonly signature: ArrayBuffer;
   readonly messages: NitroMessageV0[];
+
+  constructor(signature: ArrayBuffer, messages: NitroMessageV0[]) {
+    this.signature = signature;
+    this.messages = messages;
+    Object.freeze(this);
+  }
 }
 
 /**
@@ -234,19 +163,21 @@ export function extractNitroBatch(payload: Uint8Array): null | NitroBatchV0 {
     const nitroMessageWithMetadata =
       decodeNitroMessageWithMetadata(messageContents);
 
-    messages.push({
-      index: messageIndex,
-      length: dataSize,
-      contents: messageContents,
+    messages.push(
+      new NitroMessageV0(
+        messageIndex,
+        dataSize,
+        uint8ArrayToArrayBufferCodec.encode(messageContents),
 
-      messageWithMetadata: nitroMessageWithMetadata,
-    });
+        nitroMessageWithMetadata,
+      ),
+    );
   }
 
-  return {
-    signature,
+  return new NitroBatchV0(
+    uint8ArrayToArrayBufferCodec.encode(signature),
     messages,
-  };
+  );
 }
 
 /**
@@ -399,9 +330,9 @@ export const NitroMessageWithMetadataDisplay: React.FC<
 
       <TableLabeledValue>
         <Text text="Poster" />
-        <WalletAddressText
-          value={new WalletAddress(data.message.header.poster.buffer)}
-        />
+        <CopyWalletAddress value={data.message.header.poster}>
+          <WalletAddressText value={data.message.header.poster} />
+        </CopyWalletAddress>
       </TableLabeledValue>
 
       <TableLabeledValue>
@@ -424,6 +355,7 @@ export const NitroMessageWithMetadataDisplay: React.FC<
 
       <br />
       <Text text="L2 Message" />
+      <br />
       <br />
       <HexDumpAndCopyButtons data={data.message.l2Msg} />
     </>
