78ad371681f13e7be89cd39b7cc6af78a02dfe45
Theodore Schnepper <tschnepper@espressosys.com>
2025-10-29 13:10:44 -0600

    Add Delegation UI Site Section
    In order to start working on the Delegation UI, and to separate it
    from the Block Explorer while still being able to reuse the prebuilt
    and tested components for the Block Explorer, we require some
    level of separation.  Normally, this would facilitate a complete
    restructure of the packages into smaller packages for ease of
    consumption.  That may be a goal in the future, but to get things
    running for now, we opt to give the Delegation UI it's own section
    within the Block Explorer Components.

---
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/__docs__/delegation_ui.stories.tsx
@@ -0,0 +1,110 @@
+import { ProvideTickEverySecond } from '@/components/contexts/NowProvider';
+import { EnvironmentBanner } from '@/components/layout/environment_banner/environment_banner';
+import { Environment } from '@/models/config/environment/environment';
+import {
+  environmentArgsDecafWithContracts,
+  environmentArgsFakeDataWithContracts,
+  environmentArgsLocalDevNetWithContracts,
+  environmentArgsMainnetWithContracts,
+  environmentArgsMilkWithContracts,
+  environmentArgsWaterWithContracts,
+  environmentArgTypesWithContracts,
+} from '@/models/config/storybook/controls';
+import { StoryBookSpecifyEnvironmentAndContracts } from '@/models/config/storybook/storybook';
+import { Meta, StoryObj } from '@storybook/react-vite';
+import React from 'react';
+import { ProvideCappuccinoHotShotQueryServiceAPIContext } from '../../../pages/CappuccinoHotShotQueryServiceAPIContext';
+import DelegationUI from '../delegation_ui';
+
+interface ExampleProps {
+  environment: Environment;
+  stakeTableContractAddress?: string;
+  espTokenContractAddress?: string;
+  hotshotQueryServiceURL?: string;
+  nodeValidatorWebSocketURL?: string;
+}
+
+const Example: React.FC<ExampleProps> = ({
+  environment,
+  stakeTableContractAddress,
+  espTokenContractAddress,
+  hotshotQueryServiceURL,
+  nodeValidatorWebSocketURL,
+  ...rest
+}) => {
+  return (
+    <>
+      <StoryBookSpecifyEnvironmentAndContracts
+        environment={environment}
+        stakeTableContractAddress={stakeTableContractAddress}
+        espTokenContractAddress={espTokenContractAddress}
+        hotshotQueryServiceURL={hotshotQueryServiceURL}
+        nodeValidatorWebSocketURL={nodeValidatorWebSocketURL}
+      >
+        <EnvironmentBanner />
+        <ProvideTickEverySecond>
+          <ProvideCappuccinoHotShotQueryServiceAPIContext>
+            <DelegationUI {...rest} />
+          </ProvideCappuccinoHotShotQueryServiceAPIContext>
+        </ProvideTickEverySecond>
+      </StoryBookSpecifyEnvironmentAndContracts>
+    </>
+  );
+};
+
+const meta: Meta = {
+  title: 'Sites/Delegation UI',
+  component: Example,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {
+    environment: Environment.fakeData,
+    stakeTableContractAddress: undefined,
+    espTokenContractAddress: undefined,
+    hotshotQueryServiceURL: undefined,
+    nodeValidatorWebSocketURL: undefined,
+  },
+  argTypes: environmentArgTypesWithContracts,
+
+  globals: {
+    background: 'light',
+    parameters: {
+      backgrounds: {
+        default: 'light',
+        options: {
+          light: { name: 'Light', value: '#f8fafcff' },
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Example>;
+
+export const Default: Story = {};
+
+export const Milk: Story = {
+  args: environmentArgsMilkWithContracts,
+};
+
+export const Water: Story = {
+  args: environmentArgsWaterWithContracts,
+};
+
+export const Decaf: Story = {
+  args: environmentArgsDecafWithContracts,
+};
+
+export const Mainnet: Story = {
+  args: environmentArgsMainnetWithContracts,
+};
+
+export const FakeData: Story = {
+  args: environmentArgsFakeDataWithContracts,
+};
+
+export const LocalDevNet: Story = {
+  args: environmentArgsLocalDevNetWithContracts,
+};
---
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/__test__/delegation_ui.test.tsx
@@ -0,0 +1,13 @@
+import { composeStories } from '@storybook/react-vite';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import * as stories from '../__docs__/delegation_ui.stories';
+
+const { FakeData } = composeStories(stories);
+
+describe('Delegation UI', () => {
+  it('should smoke test render without issue', async () => {
+    render(<FakeData data-testid="1" />);
+  });
+});
