b9e7d246879568e35a6ce83e77eb1e346ee90533
Theodore Schnepper <tschnepper@espressosys.com>
2025-10-30 09:35:18 -0600

    Add a React Context for Validator Ranks and a Derivation of them
    In order to display the Rank value of different Node Validators compared to each
    other, it helps to have a definitive source of the data.  The React Context can
    be utilized to Supply the Ranks as a Map of addresses formatted as strings to
    their rank value.
    
    Additionally, a `DeriveRank` component has been added to rank Validators
    against each other utilizing the validators from the `AllValidatorContext`, and
    a pre-defined `score` function to score them.

---
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/contexts/rank_map_context.tsx
@@ -0,0 +1,61 @@
+import { stdBase64ArrayBufferCodec } from '@/convert/codec';
+import { mapIterable, zipWithIterable } from '@/functional/functional';
+import { ValidatorSetEntry } from '@/service/espresso_l1_validator_service/validators_all/validator_set_entry';
+import React from 'react';
+import { AllValidatorsContext } from './all_validators_context';
+
+/**
+ * RankMapContext provides a React Context
+ * for the current mapping of validator addresses to their ranks.
+ */
+export const RankMapContext = React.createContext<Map<string, number>>(
+  new Map(),
+);
+
+/**
+ * score computes a score for a validator set entry, this is used to derive
+ * the rank of the validators
+ */
+function score(node: ValidatorSetEntry): number {
+  return (
+    Number(node.stake) *
+    (1 - node.commission.valueOf()) *
+    (node.leadershipParticipation ?? 0.5) *
+    (node.voterParticipation ?? 0.5)
+  );
+}
+
+/**
+ * DeriveRank is a React Component that
+ * derives the current rank mapping from the all validators
+ * and provides it via the RankMapContext to its children.
+ */
+export const DeriveRank: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const allValidators = React.useContext(AllValidatorsContext);
+
+  if (!allValidators) {
+    return <>{children}</>;
+  }
+
+  const validatorsAndScore = Array.from(
+    zipWithIterable(
+      mapIterable(allValidators.nodes, (a) =>
+        stdBase64ArrayBufferCodec.encode(a.address),
+      ),
+      mapIterable(allValidators.nodes, score),
+      (a, b) => [a, b] as const,
+    ),
+  ).sort((a, b) => b[1] - a[1]);
+
+  const validatorsAndRank = new Map<string, number>(
+    validatorsAndScore.map(
+      (addressAndScore, index) => [addressAndScore[0], index + 1] as const,
+    ),
+  );
+
+  return (
+    <RankMapContext.Provider value={validatorsAndRank}>
+      {children}
+    </RankMapContext.Provider>
+  );
+};
