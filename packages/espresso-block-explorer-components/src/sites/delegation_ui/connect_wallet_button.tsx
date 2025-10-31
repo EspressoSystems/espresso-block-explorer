5115b8670934c82a987799b6675d096fab238776
Theodore Schnepper <tschnepper@espressosys.com>
2025-10-31 08:18:42 -0600

    Add Connect Wallet Button Component
    The Connect Wallet Button Component is required in order to connect the wallet.
    For convenience, we aim to connect the RainbowKit library as a multi wallet sign
    on tool.
    
    The Connect Wallet Button has two separate representations, one for when the
    Wallet is connected, and one for when it is not.

---
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/connect_wallet_button.tsx
@@ -0,0 +1,49 @@
+import {
+  RainbowKitAccountAddressContext,
+  RainbowKitModalContext,
+} from '@/components/rainbowkit/contexts/contexts';
+import Text from '@/components/text/Text';
+import WalletAddressText from '@/components/text/WalletAddressText';
+import LogOut from '@/components/visual/icons/feather/logout';
+import Plus from '@/components/visual/icons/feather/plus';
+import { hexArrayBufferCodec } from '@/convert/codec';
+import WalletAddress from '@/models/wallet_address/wallet_address';
+import React from 'react';
+import './connect_wallet_button.css';
+import ButtonLarge from './elements/buttons/button_large';
+
+/**
+ * ConnectWalletButton is a button that will open the RainbowKit Wallet Connect
+ * Modal when pressed.
+ */
+export const ConnectWalletButton: React.FC = () => {
+  const modals = React.useContext(RainbowKitModalContext);
+  const address = React.useContext(RainbowKitAccountAddressContext);
+
+  if (address) {
+    return (
+      <span>
+        <span className="wallet-address-preview">
+          <WalletAddressText
+            value={new WalletAddress(hexArrayBufferCodec.decode(address))}
+          />
+        </span>
+        &nbsp;
+        <ButtonLarge
+          className="connect-wallet connected-wallet"
+          onClick={modals.openAccountModal}
+        >
+          <Text text="Connect Wallet" />
+          <LogOut />
+        </ButtonLarge>
+      </span>
+    );
+  }
+
+  return (
+    <ButtonLarge className="connect-wallet" onClick={modals.openConnectModal}>
+      <Plus />
+      <Text text="Connect Wallet" />
+    </ButtonLarge>
+  );
+};
