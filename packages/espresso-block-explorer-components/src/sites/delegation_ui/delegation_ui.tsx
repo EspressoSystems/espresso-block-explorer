408f78e9756d71ebd01b8f5b598096be269d4927
Theodore Schnepper <tschnepper@espressosys.com>
2025-10-31 08:37:27 -0600

    Add DelegationUIContent component
    The Delegation UI Content represents a wrapping container for the content of the
    Delegation UI itself.  Since the Design indicates that the contents of the page
    should be centered, and not the same size as the rest of the Page (or header)
    We require a dedicated content element to contain and restrict the size of this
    content.
    
    Additionally, the design utilizes a dedicated Block that is blurred for stylistic
    purposes.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/delegation_ui.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/delegation_ui.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './colors.css';
+import { RetrieveActiveValidators } from './contexts/active_validators_context';
+import { RetrieveAllValidators } from './contexts/all_validators_context';
+import { DeriveConsensusSet } from './contexts/consensus_set_context';
+import { DeriveRank } from './contexts/rank_map_context';
+import { DeriveTotalStake } from './contexts/total_stake_context';
+import { ProvideTotalSupply } from './contexts/total_supply_context';
 import { DelegationHeader } from './delegation_header';
 import './delegation_ui.css';
+import { DelegationUIContent } from './delegation_ui_content';
 
 interface DelegationPageProps {
   className?: string;
@@ -15,6 +22,20 @@ const DelegationUI: React.FC<DelegationPageProps> = () => {
   return (
     <main className="delegation-ui">
       <DelegationHeader />
+
+      <RetrieveAllValidators>
+        <RetrieveActiveValidators>
+          <DeriveTotalStake>
+            <DeriveConsensusSet>
+              <DeriveRank>
+                <ProvideTotalSupply>
+                  <DelegationUIContent />
+                </ProvideTotalSupply>
+              </DeriveRank>
+            </DeriveConsensusSet>
+          </DeriveTotalStake>
+        </RetrieveActiveValidators>
+      </RetrieveAllValidators>
     </main>
   );
 };
