6ccacc6f9235b13287a4dc7fa3eb5a31bcfae70a
Theodore Schnepper <tschnepper@espressosys.com>
2025-12-08 11:26:02 -0700

    Cleanup ValidatorDisplayArea component
    Instead of utilizing a ternary operator to determine which component to display,
    we have opted to split this up for better readability / more obvious logical
    flow / complexity.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/staking_modal/validator_display_area.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/staking_modal/validator_display_area.tsx
@@ -6,24 +6,35 @@ import { ValidatorNodeContext } from '../contexts/validator_node_context';
 import { ActiveConsensusChip } from '../elements/chips/active_consensus_chip';
 import { InactiveConsensusChip } from '../elements/chips/inactive_consensus_chip';
 
-export const ValidatorDisplayArea: React.FC = () => {
+/**
+ * ActiveStatusChip is a component that displays whether the validator is active
+ * in consensus or not as a Chip.
+ */
+const ActiveStatusChip: React.FC = () => {
   const validator = React.useContext(ValidatorNodeContext);
   const consensusMap = React.useContext(ConsensusMapContext);
   const formattedAddress = validator.addressText;
   const isActive = consensusMap.has(formattedAddress);
 
-  const activeChip = isActive ? (
-    <ActiveConsensusChip />
-  ) : (
-    <InactiveConsensusChip />
-  );
+  if (isActive) {
+    return <ActiveConsensusChip />;
+  }
+  return <InactiveConsensusChip />;
+};
+
+/**
+ * ValidatorDisplayArea is a component that displays some summary details and
+ * information about the targeted validator.
+ */
+export const ValidatorDisplayArea: React.FC = () => {
+  const validator = React.useContext(ValidatorNodeContext);
 
   return (
     <div className="staking-modal-validator-display-area">
       <WalletAddressText value={new WalletAddress(validator.address)} />
       <br />
       &nbsp;
-      {activeChip}
+      <ActiveStatusChip />
     </div>
   );
 };
