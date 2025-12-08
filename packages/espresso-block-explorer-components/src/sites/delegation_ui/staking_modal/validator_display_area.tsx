3a2833fbd528f364ff8e018fd35911af8cfb6588
Theodore Schnepper <tschnepper@espressosys.com>
2025-12-08 11:19:39 -0700

    Remove APY Chip from Validator Information
    Based on GTM feedback provided by Lucca
    
    Asana Task: https://app.asana.com/1/1208976916964769/project/1211758952171173/task/1212346803455545?focus=true
    
    Lucca has provided the feedback to be consistent about utilizing
    `APR` versus `APY`.  Initially the intention was to replace the
    `APY` chip value with an `APR` chip value for the specific
    validator.  However, due to the potential complication, caveats,
    and considerations needed to be conveyed for this case, the
    conversation resolved to just dropping this value per the validator.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/staking_modal/validator_display_area.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/staking_modal/validator_display_area.tsx
@@ -1,12 +1,9 @@
-import PercentageText from '@/components/text/percentage_text';
-import Text from '@/components/text/text';
 import WalletAddressText from '@/components/text/wallet_address_text';
 import WalletAddress from '@/models/wallet_address/wallet_address';
 import React from 'react';
 import { ConsensusMapContext } from '../contexts/consensus_map_context';
 import { ValidatorNodeContext } from '../contexts/validator_node_context';
 import { ActiveConsensusChip } from '../elements/chips/active_consensus_chip';
-import { APYChip } from '../elements/chips/apy_chip';
 import { InactiveConsensusChip } from '../elements/chips/inactive_consensus_chip';
 
 export const ValidatorDisplayArea: React.FC = () => {
@@ -25,11 +22,6 @@ export const ValidatorDisplayArea: React.FC = () => {
     <div className="staking-modal-validator-display-area">
       <WalletAddressText value={new WalletAddress(validator.address)} />
       <br />
-      <APYChip>
-        <PercentageText percentage={0.035} />
-        &nbsp;
-        <Text text="APY" />
-      </APYChip>
       &nbsp;
       {activeChip}
     </div>
