a14f9256491487e9ddf098cdc42407841c234c2a
Theodore Schnepper <tschnepper@espressosys.com>
2025-12-16 08:35:10 -0700

    Add Wrong Network Select component
    In order to help protect the user from accidentally submitting
    transactions to an incorrect network, we want to add a component
    to detect that the user is interacting with the wrong network.
    This component should notify the user of the wrong network,
    and guide them to correct the network selection.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/delegation_header.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/delegation_header.tsx
@@ -2,6 +2,7 @@ import EspressoLogoAndTitle from '@/visual/icons/espresso_logo_and_title';
 import React from 'react';
 import { ConnectWalletButton } from './connect_wallet_button';
 import './delegation_header.css';
+import { WrongNetworkCheck } from './wrong_network_check';
 
 interface HeaderProps {}
 
@@ -12,6 +13,7 @@ export const DelegationHeader: React.FC<HeaderProps> = (props) => {
   return (
     <header className="delegation" {...props}>
       <EspressoLogoAndTitle />
+      <WrongNetworkCheck />
       <ConnectWalletButton />
     </header>
   );
