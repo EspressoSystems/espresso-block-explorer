b73cbdea340e4ce84e31d2cc570e0c4aaec05c4c
Theodore Schnepper <tschnepper@espressosys.com>
2026-01-14 07:11:43 -0700

    Fix read from environment test for Delegation UI
    The read from environment test for the Delegation UI only contains contract
    address considerations for two of the required four contracts.  As a result
    the test isn't passing.
    
    To fix this, we need to ensure that we're testing with all four contract
    addresses.

--- packages/delegation-ui/src/helpers/__test__/read_from_env.test.ts
+++ packages/delegation-ui/src/helpers/__test__/read_from_env.test.ts
@@ -10,6 +10,8 @@ describe('Reader From ENV', () => {
       environment: Environment.decaf,
       contract_address_stake_table: null,
       contract_address_esp_token: null,
+      contract_address_reward_claim: null,
+      contract_address_light_client: null,
     });
 
     process.env.ENVIRONMENT_NAME = 'milk';
@@ -17,6 +19,8 @@ describe('Reader From ENV', () => {
       environment: Environment.milk,
       contract_address_stake_table: null,
       contract_address_esp_token: null,
+      contract_address_reward_claim: null,
+      contract_address_light_client: null,
     });
 
     process.env.ENVIRONMENT_NAME = 'water';
@@ -24,6 +28,8 @@ describe('Reader From ENV', () => {
       environment: Environment.water,
       contract_address_stake_table: null,
       contract_address_esp_token: null,
+      contract_address_reward_claim: null,
+      contract_address_light_client: null,
     });
 
     process.env.ENVIRONMENT_NAME = 'mainnet';
@@ -31,22 +37,32 @@ describe('Reader From ENV', () => {
       environment: Environment.mainnet,
       contract_address_stake_table: null,
       contract_address_esp_token: null,
+      contract_address_reward_claim: null,
+      contract_address_light_client: null,
     });
 
     process.env.CONTRACT_ADDRESS_STAKE_TABLE = '1234';
     process.env.CONTRACT_ADDRESS_ESP_TOKEN = '5678';
+    process.env.CONTRACT_ADDRESS_REWARD_CLAIM = '9012';
+    process.env.CONTRACT_ADDRESS_LIGHT_CLIENT = '3456';
     expect(readFromEnv()).to.deep.equal({
       environment: Environment.mainnet,
       contract_address_stake_table: null,
       contract_address_esp_token: null,
+      contract_address_reward_claim: null,
+      contract_address_light_client: null,
     });
 
     process.env.CONTRACT_ADDRESS_STAKE_TABLE = '0x1234';
     process.env.CONTRACT_ADDRESS_ESP_TOKEN = '0x5678';
+    process.env.CONTRACT_ADDRESS_REWARD_CLAIM = '0x9012';
+    process.env.CONTRACT_ADDRESS_LIGHT_CLIENT = '0x3456';
     expect(readFromEnv()).to.deep.equal({
       environment: Environment.mainnet,
       contract_address_stake_table: '0x1234',
       contract_address_esp_token: '0x5678',
+      contract_address_reward_claim: '0x9012',
+      contract_address_light_client: '0x3456',
     });
   });
 });
