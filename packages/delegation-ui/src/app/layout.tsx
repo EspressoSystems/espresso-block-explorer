11a42fe06dcbfda081546b8f6e4c9fd4248b6f42
Theodore Schnepper <tschnepper@espressosys.com>
2026-01-15 08:13:41 -0700

    Fix css exports
    The css exports keep changing when there are code changes.  While we
    haven't been able to resolve why these changes are happening, and don't
    seem to be arriving at a deterministic name, the best we can do for now
    until we're able to determine and fix the issue, is just to update the
    refereneces to the files.

--- packages/delegation-ui/src/app/layout.tsx
+++ packages/delegation-ui/src/app/layout.tsx
@@ -1,18 +1,15 @@
 import 'espresso-block-explorer-components/delegation-ui.css';
-import 'espresso-block-explorer-components/espresso-block-explorer-components.css';
 import React from 'react';
 // Growing pains... It's unclear why these css file code splits are occurring.
 // After spending some time to resolve / determine them without success, we're
 // just adopting them for now until we can figure out how to resolve them
 // at a later time.
-import 'espresso-block-explorer-components/cappuccino_hot_shot_query_service_api_context.css';
 import 'espresso-block-explorer-components/stake_table_v2_contract_context.css';
-import 'espresso-block-explorer-components/inscription_and_signature.css';
+import 'espresso-block-explorer-components/inscription.css';
 import { readFromEnv } from '@/helpers/read_from_env';
 
 import LayoutClientComponent from '@/client_components/layout';
 import './globals.css';
-
 
 // Force dynamic rendering to ensure environment variables are read at runtime
 export const dynamic = 'force-dynamic';
