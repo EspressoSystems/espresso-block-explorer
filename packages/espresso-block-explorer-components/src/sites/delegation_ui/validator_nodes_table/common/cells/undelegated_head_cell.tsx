8fbe042b642c571e63ef3516684a7a73b1e3d077
Theodore Schnepper <tschnepper@espressosys.com>
2026-02-10 13:46:57 -0700

    Rename "Staked" column to "Undelegated"
    Based on feedback from Fernando, and Lucca
    
    https://www.notion.so/espressosys/Undelegated-amount-not-shown-or-accounted-anywhere-2fd2431b68e980ab9ba0f5503a63b445?v=2f62431b68e980c1993b000c5eb8de14&source=copy_link
    
    The "Staked" column in the blue "pending undelegations" table doesn't
    clearly indicate that the amount specified in this column is actually
    your pending undelegated amount.
    
    This should be renamed with a info icon that clearly indicates what is
    being displayed here.

---
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/validator_nodes_table/common/cells/undelegated_head_cell.tsx
@@ -0,0 +1,18 @@
+import Text from '@/components/text/text';
+import { MoreInfoElement } from '@/sites/delegation_ui/elements/tooltip/more_info';
+
+/**
+ * UndelegatedHeadCell is the header cell for the validator table that
+ * represents the amount the wallet has actively pending withdrawl from the
+ * validator node.
+ */
+export const UndelegatedHeadCell: React.FC = () => {
+  return (
+    <>
+      <Text text="Undelegated" />
+      <MoreInfoElement>
+        <Text text="You will be able to claim this amount after the escrow period ends" />
+      </MoreInfoElement>
+    </>
+  );
+};
