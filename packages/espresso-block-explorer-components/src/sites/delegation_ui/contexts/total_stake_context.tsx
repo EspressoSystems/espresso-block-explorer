97f897e438ec208d4f4a366cd985a344fb094de4
Theodore Schnepper <tschnepper@espressosys.com>
2026-04-13 12:19:59 -0600

    Add leading space formatting for percentages where needed
    Asana Task:
    https://app.asana.com/1/1208976916964769/project/1210224252082711/task/1214024316954861?focus=true
    
    With the implementation of the alignment, we want to take steps to
    ensure that we attempt to align these numbers together.  So we want to
    take steps to ensure alignment happens when certain scenarios align that
    would make them fall out of alignment.
    
    To account for this, we detect when the misalignment will occur due to
    any single node having greater than 10% stake, and when this occurs, we
    optionally add a leading space to the percentages that fall below this
    number.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/contexts/total_stake_context.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/contexts/total_stake_context.tsx
@@ -10,6 +10,22 @@ import { FullNodeSetSnapshotContext } from './full_node_set_snapshot_context';
 export const TotalStakeContext = React.createContext<bigint>(0n);
 
 /**
+ * LargestNodeStakeContext provides a React Context for the largest stake among
+ * all of the full node set.
+ */
+export const LargestNodeStakeContext = React.createContext<bigint>(0n);
+
+/**
+ * LargestNodeHasOver10PercentageContext is a context that is used as a quickly
+ * referencable flag, that indicates whether a single validator node controls
+ * at least 10% of the stake.
+ *
+ * This is utilized for formatting alignment purposes at the monent.
+ */
+export const LargestNodeHasOver10PercentageContext =
+  React.createContext<boolean>(false);
+
+/**
  * DeriveTotalStake is a component that Provides the TotalStakeContext
  * by calculating the total stake from the AllValidatorsContext.
  */
@@ -32,10 +48,27 @@ const DeriveTotalStakeFromAllValidators: React.FC<React.PropsWithChildren> = ({
     0n,
     fullNodeSet?.nodes ?? emptyIterator<NodeSetEntry>(),
   );
+  const largestStake = foldRIterable(
+    (largestStake: bigint, node) => {
+      if (largestStake < node.stake) {
+        return node.stake;
+      }
+
+      return largestStake;
+    },
+    0n,
+    fullNodeSet?.nodes ?? emptyIterator<NodeSetEntry>(),
+  );
 
   return (
     <TotalStakeContext.Provider value={totalStake}>
-      {children}
+      <LargestNodeStakeContext.Provider value={largestStake}>
+        <LargestNodeHasOver10PercentageContext.Provider
+          value={Number(largestStake) / Number(totalStake) >= 0.1}
+        >
+          {children}
+        </LargestNodeHasOver10PercentageContext.Provider>
+      </LargestNodeStakeContext.Provider>
     </TotalStakeContext.Provider>
   );
 };
