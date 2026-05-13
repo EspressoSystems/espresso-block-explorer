2b28def1d882fbfaf57d366b68e2eb3ce458d10e
Theodore Schnepper <tschnepper@espressosys.com>
2026-05-13 12:39:47 -0600

    Add fallback support for Pending Claims
    Just like with the `Pending Validator Exits` the `Pending Claim
    Withdrawls` can apparently also be absent from the `Stake Table` and
    `All Validators` list.
    As a result, some users may be missing data that would prevent them from
    utilzing the `Delegation UI` from withdrawing claimed funds.
    
    This aims to ensure that the `Pending Claims` entries are all available
    in the local list.

--- packages/espresso-block-explorer-components/src/sites/delegation_ui/pending_claims.tsx
+++ packages/espresso-block-explorer-components/src/sites/delegation_ui/pending_claims.tsx
@@ -1,10 +1,17 @@
 import { Text } from '@/components/text';
 import { PadlockSquare2 } from '@/components/visual/icons/sharp_line';
 import { Now } from '@/contexts/now_provider';
+import { hexArrayBufferCodec } from '@/convert/codec/array_buffer_hex';
 import { compareIterables } from '@/functional/functional';
+import { TaggedBase64 } from '@/models/espresso/tagged_base64/tagged_base64';
+import { NodeSetEntry } from '@/service/espresso_staking_api_service/common/node_set_entry';
+import { RatioRational } from '@/service/espresso_staking_api_service/common/ratio';
 import { default as React } from 'react';
 import { CollapsableHeader, CollapsableSection } from './collapsable_section';
-import { NodeAddressListContext } from './contexts/all_validators_context';
+import {
+  AllValidatorsContext,
+  NodeAddressListContext,
+} from './contexts/all_validators_context';
 import { CollapseState } from './contexts/collapse_context';
 import { PendingUndelegationsContext } from './contexts/pending_undelegations_context';
 import './pending_claims.css';
@@ -30,6 +37,54 @@ export const PendingClaims: React.FC = () => {
 };
 
 /**
+ * EnsureValidatorEntries is a component that ensures that we have entries
+ * present in the `AllValidatorsContext` for validator entries that might
+ * be missing.
+ *
+ * This is specifically to account for the Pending Claims, which will no longer
+ * have a nodes entry in the `AllValidatorsContext` this is not expected, but
+ * can still occur.
+ */
+const EnsureValidatorEntries: React.FC<React.PropsWithChildren> = ({
+  children,
+}) => {
+  const allValidators = React.useContext(AllValidatorsContext);
+  const nodeAddressList = React.useContext(NodeAddressListContext);
+
+  const nextAllValidators = React.useMemo(() => {
+    const next = new Map(allValidators);
+
+    for (const address of nodeAddressList) {
+      const found = next.get(address);
+      if (found) {
+        // Already present, ignore
+        continue;
+      }
+
+      // We need to create a placeholder for the validator
+      next.set(
+        address,
+        new NodeSetEntry(
+          hexArrayBufferCodec.decode(address),
+          new TaggedBase64('', new ArrayBuffer(0)),
+          0n,
+          new RatioRational(0n, 1n),
+          null,
+        ),
+      );
+    }
+
+    return next;
+  }, [allValidators, nodeAddressList]);
+
+  return (
+    <AllValidatorsContext.Provider value={nextAllValidators}>
+      {children}
+    </AllValidatorsContext.Provider>
+  );
+};
+
+/**
  * FilterToAvailablePendingExits is a component that filters the pending exits
  * to only include those that are available to be claimed.
  */
@@ -40,26 +95,26 @@ const FilterToAvailablePendingClaims: React.FC<React.PropsWithChildren> = ({
   const nodeAddressList = React.useContext(NodeAddressListContext);
   const now = React.useContext(Now);
 
-  const [pendingExitsList, setPendingExitsList] = React.useState<
+  const [pendingClaimsList, setPendingClaimsList] = React.useState<
     `0x${string}`[]
   >([]);
 
   React.useEffect(() => {
-    let setNextPendingExitsList = setPendingExitsList;
-    const nextPendingExitsList = Array.from(pendingClaims.keys());
+    let setNextPendingExitsList = setPendingClaimsList;
+    const nextPendingClaimsList = Array.from(pendingClaims.keys());
 
-    if (compareIterables(pendingExitsList, nextPendingExitsList) !== 0) {
-      setNextPendingExitsList(nextPendingExitsList);
+    if (compareIterables(pendingClaimsList, nextPendingClaimsList) !== 0) {
+      setNextPendingExitsList(nextPendingClaimsList);
     }
 
     return () => {
       setNextPendingExitsList = () => {};
     };
-  }, [nodeAddressList, now, pendingClaims, pendingExitsList]);
+  }, [nodeAddressList, now, pendingClaims, pendingClaimsList]);
 
   return (
-    <NodeAddressListContext.Provider value={pendingExitsList}>
-      {children}
+    <NodeAddressListContext.Provider value={pendingClaimsList}>
+      <EnsureValidatorEntries>{children}</EnsureValidatorEntries>
     </NodeAddressListContext.Provider>
   );
 };
