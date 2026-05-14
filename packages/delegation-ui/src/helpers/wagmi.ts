3dc7ab482c37c790aced11af1fcbacb19dd9d4fb
Theodore Schnepper <tschnepper@espressosys.com>
2026-05-14 07:37:17 -0600

    Revert url adjustments for delegation-ui, update storybook mainnet urls

--- packages/delegation-ui/src/helpers/wagmi.ts
+++ packages/delegation-ui/src/helpers/wagmi.ts
@@ -12,11 +12,7 @@ function httpTransports(urls: readonly string[]) {
 export const mainnet: CreateConfigParameters = {
   chains: [chains.mainnet],
   transports: {
-    [chains.mainnet.id]: httpTransports([
-      'https://rpc.flashbots.net/',
-      'https://eth.drpc.org/',
-      'https://rpc.mevblocker.io/',
-    ]),
+    [chains.mainnet.id]: httpTransports(chains.mainnet.rpcUrls.default.http),
   },
 };
 
