6fa60b6a4be672c12f6bd855e3047439afae8aa6
Mat R. <1577341+Ancient123@users.noreply.github.com>
2026-07-22 14:37:28 -0600

    Fix more tests

--- packages/delegation-ui/src/helpers/__test__/wagmi.test.ts
+++ packages/delegation-ui/src/helpers/__test__/wagmi.test.ts
@@ -119,8 +119,8 @@ describe('Wagmi Configuration', () => {
     it('should have correct chain IDs', () => {
       expect(mainnet.chains[0].id).toBe(1);
       expect(decaf.chains[0].id).toBe(11155111);
-      expect(water.chains[0].id).toBe(900);
-      expect(milk.chains[0].id).toBe(900);
+      expect(water.chains[0].id).toBe(1337);
+      expect(milk.chains[0].id).toBe(1337);
       expect(localDevNet.chains[0].id).toBe(31337);
       expect(fakeData.chains[0].id).toBe(31337);
     });
