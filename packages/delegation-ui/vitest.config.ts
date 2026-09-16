cd12d93e331f91f67354765be5f9d14a41358031
Mat Richmond <ancients@espressosys.com>
2026-09-16 10:18:25 -0600

    Updates and vitest optimization
    Update globals
    vitest optimization
    update typescript
    update vite plugins

--- packages/delegation-ui/vitest.config.ts
+++ packages/delegation-ui/vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     globalSetup: './vitest.global-setup.ts',
+    pool: 'vmThreads',
     environment: 'jsdom',
     setupFiles: './setupTests.ts',
     css: true,
